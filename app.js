858f911fe95db9c4782b2318e1ae996a0f293821
naji2018 <53320482+naji2018@users.noreply.github.com>
2022-07-04 04:35:02 +0200

    create validate() function

--- app.js
+++ app.js
@@ -6,17 +6,53 @@ function validate() {
 
   checkusername(username);
   checkemail(email);
-  checkpassword(password, cpassword);
+  checkpassword(password);
+  checkpasswordmatch(password, cpassword);
 }
 
 function checkusername(username) {
   if (username.length > 7) {
     document.getElementById("username").classList.add("success");
     document.getElementById("username").classList.replace("error", "success");
-    document.getElementById("username").innerHTML = "";
+    document.getElementById("username_error").innerHTML = "";
   } else {
     document.getElementById("username").classList.add("error");
     document.getElementById("username_error").innerText =
       "username must be 8 letters long";
+  }
+}
+
+function checkemail(email) {
+  if (email.length > 8 && email.includes("@")) {
+    document.getElementById("email").classList.add("success");
+    document.getElementById("email").classList.replace("error", "success");
+    document.getElementById("email_error").innerHTML = "";
+  } else {
+    document.getElementById("email").classList.add("error");
+    document.getElementById("email_error").innerText = "email must include @";
+  }
+}
+
+function checkpassword(password) {
+  if (password.length > 7 && password.includes(".")) {
+    document.getElementById("password").classList.add("success");
+    document.getElementById("password").classList.replace("error", "success");
+    document.getElementById("password_error").innerHTML = "";
+  } else {
+    document.getElementById("password").classList.add("error");
+    document.getElementById("password_error").innerText =
+      "password must include .";
+  }
+}
+
+function checkpasswordmatch(password, cpassword) {
+  if (password === cpassword) {
+    document.getElementById("cpassword").classList.add("success");
+    document.getElementById("cpassword").classList.replace("error", "success");
+    document.getElementById("password_error").innerHTML = " ";
+  } else {
+    document.getElementById("cpassword").classList.add("error");
+    document.getElementById("cpassword_error").innerText =
+      "password not matched";
   }
 }
